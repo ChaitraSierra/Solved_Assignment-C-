bf8424ca45cd2a2f7d9e341f87e7dc3df6b27fcf
ChaitraSierra <Chaitra@sierralearningacademy.com>
2025-07-22 15:32:15 +0530

    Create Session1.cs

---
+++ Session1.cs
@@ -0,0 +1,21 @@
+using System;
+ 
+ 
+ 
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Prompt the user to enter the meter serial number
+        Console.Write("Enter the meter serial number: ");
+ 
+        // Read user input from the console
+        string meterSerialNumber = Console.ReadLine();
+ 
+        // Display the entered serial number
+        Console.WriteLine("Meter Serial Number: " + meterSerialNumber);
+ 
+ 
+ 
+    }
+}
